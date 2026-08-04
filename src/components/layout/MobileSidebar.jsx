2268dd7f9ca2b63eb9a19bf7920ad1a160f8b15b
zeko1104 <zsukurlu@gmail.com>
2026-08-04 18:46:37 +0400

    add search MobileSidebar

--- src/components/layout/MobileSidebar.jsx
+++ src/components/layout/MobileSidebar.jsx
@@ -1,43 +1,42 @@
 import React from "react";
 import Input from "../common/Input";
 import { IoMdClose } from "react-icons/io";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const MobileSideBar = ({ isOpen, onClose }) => {
   return (
     <>
-      {/* 1. BACKGROUND  OVERLAY \ */}
+      {/* 1. BACKGROUND OVERLAY */}
       <div
         onClick={onClose}
         className={`fixed inset-0 bg-black/50 z-40 transition-opacity duration-300 md:hidden ${
-          isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+          isOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
         }`}
       />
 
-      {/* 2. SİDEBAR PANEL  */}
+      {/* 2. SIDEBAR PANEL */}
       <aside
-        className={`fixed top-0 left-0 h-full w-[80%] max-w-[320px] bg-white z-50 p-5 shadow-xl transition-transform duration-300 ease-in-out md:hidden flex flex-col justify-between ${
+        className={`fixed top-0 left-0 h-full w-[80%] max-w-[320px] bg-white z-50 p-5 shadow-xl transition-transform duration-300 ease-in-out md:hidden flex flex-col justify-between overflow-y-auto ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
         <div>
-          {/* UP SİDE*/}
-          <div className="flex justify-between items-center pb-4 mb-4 border-b border-gray-200">
-            <h2 className="text-xl font-bold text-gray-800">ZEKOWOOD</h2>
-            <button
-              onClick={onClose}
-              className="text-2xl text-gray-600 hover:text-black p-1 cursor-pointer"
-              aria-label="Close Menu"
-            >
-              <IoMdClose />
-            </button>
+          {/* UP SIDE: Search Input */}
+          <div className="flex justify-between items-center gap-2 pb-4 border-b border-gray-200">
+            {/* Search Input and Button Container */}
+            <div className="flex w-full min-w-0">
+              <div className="flex-1 min-w-0">
+                <Input placeholder="Search Products..." />
+              </div>
+              <button className="border border-l-0 border-[#D9D9D9] p-2 w-10  text-center flex justify-center items-center shrink-0">
+                <FaMagnifyingGlass className="text-gray-500 cursor-pointer" />
+              </button>
+            </div>
           </div>
 
-          {/* Axtarış Hissəsi */}
-          {/* <div className="my-4">
-            <Input placeholder="Məhsul axtar..." />
-          </div> */}
-
-          {/* NAV LİNK*/}
+          {/* NAV LINK */}
           <nav className="mt-6">
             <ul className="flex flex-col gap-4 font-medium text-gray-700">
               <li>
@@ -64,7 +63,6 @@ const MobileSideBar = ({ isOpen, onClose }) => {
           </nav>
         </div>
 
-        
         <div className="pt-4 border-t border-gray-200 text-sm text-gray-500">
           <p>© 2026 ZEKOWOOD.</p>
         </div>
